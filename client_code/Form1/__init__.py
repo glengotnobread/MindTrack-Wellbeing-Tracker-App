744447357fa13e98269e96b4860089af53a82245
fer0039@nwsc.vic.edu.au <fer0039@nwsc.vic.edu.au>
2025-07-01 00:20:38 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def column_panel_1_show(self, **event_args):
+    """This method is called when the column panel is shown on the screen"""
+    pass
